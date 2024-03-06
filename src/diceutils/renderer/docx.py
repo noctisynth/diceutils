ab35216f17ab521e977958d35c5b8d3c63cb2266
KT101 <66347108+absdf15@users.noreply.github.com>
2024-03-06 18:30:09 +0800

    to(renderer_docx): solve chinese font issue in Docx by creating new styles

--- src/diceutils/renderer/docx.py
+++ src/diceutils/renderer/docx.py
@@ -1,35 +1,52 @@
-# class DocxRenderer(Renderer):
-#     document: DocumentType
+from typing import Optional
+from diceutils.renderer._models import Renderer, Message
 
-#     def __init__(self) -> None:
-#         self.document = Document()
-#         self.document.styles["Normal"].font.name = "SimSun"
+from pathlib import Path
 
-#     def export(self) -> None:
-#         self.document.save("a.docx")
+from docx.enum.style import WD_STYLE_TYPE
+from docx import Document
+from docx.shared import Pt
+from docx.oxml.ns import qn
 
-#     def render_message(self, message: Message) -> None:
-#         paragraph = self.document.add_paragraph()
-#         for element in message.elements:
-#             if element.type == "text":
-#                 if element.tag == "act":
-#                     run = paragraph.add_run(element.content)
-#                     run.bold = True
-#                     run.font.name = "SimSun"
-#                 elif element.tag == "outside":
-#                     paragraph.add_run("（")
-#                     content = element.content.strip("（）()")
-#                     run = paragraph.add_run(content)
-#                     run.font.color.rgb = (207, 210, 210)
-#                     run.font.name = "SimSun"
-#                 elif element.tag == "speak":
-#                     content = element.content.strip('“”""')
-#                     paragraph.add_run("“")
-#                     run = paragraph.add_run(content)
-#                     run.font.name = "Microsoft YaHei"
-#                     paragraph.add_run("”")
-#             else:
-#                 # self.document.add_picture()
-#                 ...
 
-#         return
+class DocxRenderer(Renderer):
+
+    def __init__(self) -> None:
+        self.document = Document()
+        self._add_chinese_font_style("微软雅黑")
+        self._add_chinese_font_style("SimSun", "SimSun-ExtB")
+
+    def _add_chinese_font_style(self, style_name: str, font_name: Optional[str] = None, font_size: float = 11.0):
+        font_name = font_name or style_name
+        style_song = self.document.styles.add_style(style_name, WD_STYLE_TYPE.CHARACTER)
+        style_song.font.name = font_name
+        style_song.font.size = Pt(font_size)
+        style_song.element.rPr.rFonts.set(qn('w:eastAsia'), font_name)
+
+    def export(self, filename: str) -> Path:
+        filepath = Path("./exports/" + filename + ".docx").resolve()
+
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        self.document.save(str(filepath))
+
+        return filepath
+
+    def render_message(self, message: Message) -> None:
+        paragraph = self.document.add_paragraph()
+        for element in message.elements:
+            if element.type == "text":
+                if element.tag == "act":
+                    run = paragraph.add_run(element.content, style="SimSun")
+
+                elif element.tag == "outside":
+                    content = element.content.strip("（）()")
+                    run = paragraph.add_run(content, style="SimSun")
+
+                elif element.tag == "speak":
+                    run = paragraph.add_run(f"“{element.content}”", style="微软雅黑")
+
+            else:
+                # self.document.add_picture()
+                ...
+
+        return
